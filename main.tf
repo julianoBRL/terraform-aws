727bd3b17346fc73bae1b54aa548ceb22fdbae07
Juliano Lira <juliano0forum@gmail.com>
2024-10-29 12:44:08 -0300

    Renomeado deadletter

--- main.tf
+++ main.tf
@@ -8,18 +8,18 @@ provider "aws" {
 # DEADLETTER QUEUE
 #
 ######################################
-resource "aws_sqs_queue" "sqs_terraform_demo_dl_queue" {
-  name                          = var.sqs_terraform_demo_dl_queue_name
+resource "aws_sqs_queue" "sqs_terraform_demo_queue_deadletter" {
+  name                          = var.sqs_terraform_demo_queue_deadletter_name
   message_retention_seconds     = 86400
   fifo_queue                    = true
   content_based_deduplication   = true
-  deduplication_scope           = var.sqs_terraform_demo_dl_queue_deduplication_scope
+  deduplication_scope           = var.sqs_terraform_demo_queue_deadletter_deduplication_scope
   fifo_throughput_limit         = "perMessageGroupId"
 }
 
-resource "aws_sqs_queue_policy" "sqs_terraform_demo_dl_queue_policy" {
-  queue_url = aws_sqs_queue.sqs_terraform_demo_dl_queue.id
-  policy = data.aws_iam_policy_document.sqs_terraform_demo_dl_queue_policy_document.json
+resource "aws_sqs_queue_policy" "sqs_terraform_demo_queue_deadletter_policy" {
+  queue_url = aws_sqs_queue.sqs_terraform_demo_queue_deadletter.id
+  policy = data.aws_iam_policy_document.sqs_terraform_demo_queue_deadletter_policy_document.json
 }
 
 
@@ -39,7 +39,7 @@ resource "aws_sqs_queue" "sqs_terraform_demo_queue" {
   deduplication_scope           = var.sqs_terraform_demo_queue_deduplication_scope
   fifo_throughput_limit         = "perMessageGroupId"
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.sqs_terraform_demo_dl_queue.arn
+    deadLetterTargetArn = aws_sqs_queue.sqs_terraform_demo_queue_deadletter.arn
     maxReceiveCount     = 4
   })
 }
--- variables.tf
+++ variables.tf
@@ -14,13 +14,13 @@ variable "sqs_terraform_demo_queue_deduplication_scope" {
 
 
 
-variable "sqs_terraform_demo_dl_queue_name" {
+variable "sqs_terraform_demo_queue_deadletter_name" {
   description = "Nome da fila SQS"
   type = string
-  default = "sqs_terraform_demo_dl_queue"
+  default = "sqs_terraform_demo_queue_deadletter"
 }
 
-variable "sqs_terraform_demo_dl_queue_deduplication_scope" {
+variable "sqs_terraform_demo_queue_deadletter_deduplication_scope" {
   description = "Como ele vai fazer a deduplicação queue/messageGroup"
   type = string
   default = "messageGroup"
