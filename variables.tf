b3282fbc527fc951ae870e20b7c7e0581ce143cd
Juliano Lira <juliano0forum@gmail.com>
2024-10-29 12:46:17 -0300

    Renomeado deadletter

--- variables.tf
+++ variables.tf
@@ -14,13 +14,13 @@ variable "sqs_terraform_demo_queue_deduplication_scope" {
 
 
 
-variable "sqs_terraform_demo_queue_deadletter_name" {
+variable "sqs_terraform_demo_queue_dl_name" {
   description = "Nome da fila SQS"
   type = string
-  default = "sqs_terraform_demo_queue_deadletter"
+  default = "sqs_terraform_demo_queue_dl"
 }
 
-variable "sqs_terraform_demo_queue_deadletter_deduplication_scope" {
+variable "sqs_terraform_demo_queue_dl_deduplication_scope" {
   description = "Como ele vai fazer a deduplicação queue/messageGroup"
   type = string
   default = "messageGroup"
